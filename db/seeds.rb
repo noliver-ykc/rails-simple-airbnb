c76779df0e37af4a0db90f673429f997b2fe6cab
Nicole Olliver <noliver.ykc@gmail.com>
2022-07-07 22:58:30 +0900

    finished fake airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+adjective = ["Beautiful", "Spacious", "Luxurious","Stylish","Lush"]
+room_type = ["Villa","Apartment","Hotel"]
+city = ["London", "Tokyo", "Seoul", "Paris"]
+
+5.times do
+  Flat.create!(
+    name: "#{adjective.sample} #{room_type.sample} in #{city.sample}",
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence(word_count: 23),
+    price_per_night: rand(3000..20000),
+    number_of_guests: rand(2..10)
+  )
+end
+puts "Finished!"
